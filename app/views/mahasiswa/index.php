4b8828d86ab88d2657408ee905ad746f1a22a0ab
Hasbi-Assidiqqi <assidiqqi.0210.com>
2023-06-29 22:08:55 +0700

    menambahkan fitur hapus

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -17,9 +17,10 @@
 
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
-                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <li class="list-group-item">
                         <?= $mhs['nama'] ?>
-                        <a href="<?= BASEURL; ?>mahasiswa/detail/<?= $mhs['id'] ?>" class="badge text-bg-primary">Detail</a>
+                        <a href="<?= BASEURL; ?>mahasiswa/hapus/<?= $mhs['id'] ?>" class="badge text-bg-danger float-end" onclick="return confirm('Yakin ingin menghapus data?');">Hapus</a>
+                        <a href="<?= BASEURL; ?>mahasiswa/detail/<?= $mhs['id'] ?>" class="badge text-bg-primary float-end me-1">Detail</a>
                     </li>
                 <?php endforeach ?>
             </ul>
